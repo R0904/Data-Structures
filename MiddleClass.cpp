78406bbe1f288204f1edc7b4fb7ce07fc73db1f7
R0904 <60311091+R0904@users.noreply.github.com>
2020-05-14 23:39:10 +0530

    Update MiddleClass.cpp

--- MiddleClass.cpp
+++ MiddleClass.cpp
@@ -2,12 +2,6 @@
 using namespace std;
 const int MOD = 1000000007;
 const int MAX = 100005;
-const double PI = acos(-1.0);
-
-int SetBit (int n, int X) { return n | (1 << X); }
-int ClearBit (int n, int X) { return n & ~(1 << X); }
-int ToggleBit (int n, int X) { return n ^ (1 << X); }
-bool CheckBit (int n, int X) { return (bool)(n & (1 << X)); }
 
 double arr[MAX];
 
